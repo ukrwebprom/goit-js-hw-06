75e679a414824756fc3295bbbb5a69a8e760f902
ukrwebprom <ukrwebprom@gmail.com>
2022-12-13 13:41:55 +0200

    tasks 1-3

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ingredientsList = document.querySelector('#ingredients');
+
+const makeLiElement = (ingredient) => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add('item');
+  return ingredientEl;
+}
+const liElements = ingredients.map(makeLiElement);
+ingredientsList.append(...liElements);
