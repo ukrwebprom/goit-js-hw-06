c126a4550665dd83b740a5ca419ecbcd96d5c1cc
ukrwebprom <ukrwebprom@gmail.com>
2022-12-14 17:00:03 +0200

    tasks 9 -10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+const onBtnClick = () => {
+  const rndColor = getRandomHexColor();
+  spanEl.textContent = rndColor;
+  document.body.style.backgroundColor = rndColor;
+}
+btn.addEventListener('click', onBtnClick);
